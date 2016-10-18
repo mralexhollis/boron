3f74784ba77bdac618b3011756876596d4bac24e
Alex Hollis <mr.alex.hollis@gmail.com>
2016-10-18 11:38:06 +0100

    Adding more base files

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ var appRoutes = require('./routes/app');
 var messageRoutes = require('./routes/messages');
 
 var app = express();
-mongoose.connect('localhost:27017/node-angular');
+const MONGOURI = process.env.MONGODB_URI;
+
+mongoose.connect(MONGOURI + '/node-angular');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
